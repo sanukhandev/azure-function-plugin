ee281c4bcb7b7e48275c4c37c3c264f06fd4d831
GitHub Action <action@github.com>
2023-01-02 11:51:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/sanukhandev/azurefunctionplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/sanukhandev/azurefunctionplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sanukhandev.azurefunctionplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sanukhandev.azurefunctionplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/sanukhandev/azurefunctionplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sanukhandev/azurefunctionplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sanukhandev.azurefunctionplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sanukhandev.azurefunctionplugin.MyBundle
 
 class MyApplicationService {
 
